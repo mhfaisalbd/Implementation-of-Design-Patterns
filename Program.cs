2fdd9016c0811b2e288dad61e3682774c849b87e
mhfaisalbd <bdmhfaisal@gmail.com>
2020-02-04 18:25:04 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using DesignPattern.Singleton;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(FirstExpression, SecondExpression);
+            Console.ReadKey();
+        }
+
+        static void FirstExpression()
+        {
+            SingletonImplementation singleton = SingletonImplementation.GetSingleton;
+            singleton.ExpressThis("First");
+        }
+        static void SecondExpression()
+        {
+            SingletonImplementation singleton = SingletonImplementation.GetSingleton;
+            singleton.ExpressThis("Second");
+        }
+    }
+}
---
+++ Singleton/SingletonImplementation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPattern.Singleton
+{
+    public sealed class SingletonImplementation
+    {
+        private static readonly Lazy<SingletonImplementation> Instance = new Lazy<SingletonImplementation>(()=>new SingletonImplementation());
+        private readonly int _counter = 0;
+        public static SingletonImplementation GetSingleton => Instance.Value;
+
+        private SingletonImplementation()
+        {
+            _counter++;
+            Console.WriteLine(_counter);
+        }
+
+        public void ExpressThis(string expression)
+        {
+            Console.WriteLine($"Expressed from {expression} by Instance {_counter}");
+        }
+    }
+}
